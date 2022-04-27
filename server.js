d96a648c5be0cad2a8fd577824f15eb96dc6a7bc
Alex_Wang <k8cchaha@gmail.com>
2022-04-27 10:23:24 +0800

    add mongoose

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/hotel")
+  .then(() => {
+    console.log("成功連線");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const roomSchema = {
+  name: String,
+  price: {
+    type: Number,
+    require: [true, "價格必填"],
+  },
+  rating: Number,
+};
+
+const Room = mongoose.model("Room", roomSchema);
+
+const testRoom = new Room({
+  name: "超級單人房",
+  price: 3000,
+  rating: 4.4,
+});
+
+testRoom
+  .save()
+  .then(() => {
+    console.log("新增成功");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const requestListener = (req, res) => {
   console.log(req.url);
